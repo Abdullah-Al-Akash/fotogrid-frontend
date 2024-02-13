d0a5924f9dbfd8133ba6c90b03f2448bfeaf7770
Ashiqur Rahman Biplop <biplops22@gmail.com>
2024-02-13 17:00:18 +0600

    added 2 section

--- src/Components/Events/ResultTable.jsx
+++ src/Components/Events/ResultTable.jsx
@@ -2,52 +2,52 @@ import resultTableImage from "../../../public/Images/Ellipse 1.png";
 import trophy from "../../../public/Images/trophy-01.png";
 const ResultTable = () => {
   return (
-    <div className="overflow-x-auto border-t !rounded-[6px]">
+    <div className="overflow-x-auto !rounded-[6px] border">
       <table className="table !font-medium ">
         {/* head */}
         <thead>
-          <tr className="bg-base-200 !text-[16px] !montserrat">
-            <th className="border-r border-s border-b">Club</th>
-            <th className="border-r border-b">Total Plays</th>
-            <th className="border-r border-b">Total Wins</th>
-            <th className="border-r border-b">Total Lose</th>
-            <th className="border-r border-b">Points For</th>
-            <th className="border-r border-b">Points Against</th>
-            <th className="border-r border-b">Points Differences</th>
-            <th className="border-r border-b">Final Standings</th>
+          <tr className="bg-base-200 !text-[16px] !montserrat ">
+            <th className="border-r">Club</th>
+            <th className="border-r">Total Plays</th>
+            <th className="border-r">Total Wins</th>
+            <th className="border-r">Total Lose</th>
+            <th className="border-r">Points For</th>
+            <th className="border-r">Points Against</th>
+            <th className="border-r">Points Differences</th>
+            <th className="">Final Standings</th>
           </tr>
         </thead>
         <tbody>
           {/* row 1 */}
-          <tr>
-            <th className="flex items-center gap-[8px] border-r border-s border-b">
+          <tr className="">
+            <th className="flex flex-col lg:flex-row items-center gap-[8px] border-r">
               <img src={resultTableImage} className="w-[28] h-[28]" alt="" />
               <h1>Victoria</h1>
             </th>
-            <td className="border-r border-b">10</td>
-            <td className="border-r border-b">10</td>
-            <td className="border-r border-b">0</td>
-            <td className="border-r border-b">100</td>
-            <td className="border-r border-b">50</td>
-            <td className="border-r border-b">50</td>
-            <td className="flex items-center gap-[8px] border-r">
+            <td className="border-r">10</td>
+            <td className="border-r">10</td>
+            <td className="border-r">0</td>
+            <td className="border-r">100</td>
+            <td className="border-r">50</td>
+            <td className="border-r">50</td>
+            <td className="flex flex-col md:flex-row items-center gap-[8px] ">
               <img src={trophy} className="w-[20px] h-[20px]" alt="" />
               <p className="text-[16px]">Winner</p>
             </td>
           </tr>
           {/* row 1 */}
-          <tr>
-            <th className="flex items-center gap-[8px]  border-r border-s border-b">
+          <tr className="">
+            <th className="flex flex-col lg:flex-row items-center gap-[8px]  border-r">
               <img src={resultTableImage} className="w-[28] h-[28]" alt="" />
               <h1>Victoria</h1>
             </th>
-            <td className="border-r border-b">10</td>
-            <td className="border-r border-b">10</td>
-            <td className="border-r border-b">0</td>
-            <td className="border-r border-b">100</td>
-            <td className="border-r border-b">50</td>
-            <td className="border-r border-b">50</td>
-            <td className="flex items-center gap-[8px]  border-b border-r">
+            <td className="border-r">10</td>
+            <td className="border-r">10</td>
+            <td className="border-r">0</td>
+            <td className="border-r">100</td>
+            <td className="border-r">50</td>
+            <td className="border-r">50</td>
+            <td className="flex flex-col md:flex-row items-center gap-[8px]">
               <img src={trophy} className="w-[20px] h-[20px]" alt="" />
               <p className="text-[16px]">Winner</p>
             </td>
