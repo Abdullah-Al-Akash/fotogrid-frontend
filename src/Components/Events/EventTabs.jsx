4c989365628f115a99d1441a4dbb6a4ab48d1491
Ashiqur Rahman Biplop <biplops22@gmail.com>
2024-02-13 17:11:13 +0600

    solve tab issue

--- src/Components/Events/EventTabs.jsx
+++ src/Components/Events/EventTabs.jsx
@@ -17,45 +17,45 @@ const EventTabs = ({ tab, setTab }) => {
             <div className="md:flex hidden items-start gap-[16px] px-[24px] text-[20px] text-[#7D7D7D] rubik ">
               <p
                 onClick={() => setTab("tab1")}
-                className={`py-[12px] px-[20px] cursor-pointer ${
-                  tab === "tab1" &&
-                  "text-[#A46E40] border-b border-[#A46E40] font-medium"
+                className={`py-[12px] px-[20px] cursor-pointer duration-200   ${
+                  tab === "tab1"  ?
+                  "text-[#A46E40] border-b border-[#A46E40] font-medium" : "border-b border-transparent font-medium"
                 }`}
               >
                 Results & Standing
               </p>
               <p
                 onClick={() => setTab("tab2")}
-                className={`py-[12px] px-[20px] cursor-pointer ${
-                  tab === "tab2" &&
-                  "text-[#A46E40] border-b border-[#A46E40] font-medium"
+                className={`py-[12px] px-[20px] cursor-pointer duration-200  ${
+                  tab === "tab2"  ?
+                  "text-[#A46E40] border-b border-[#A46E40] font-medium" : "border-b border-transparent font-medium"
                 }`}
               >
                 Event Info & News
               </p>
               <p
                 onClick={() => setTab("tab3")}
-                className={`py-[12px] px-[20px] cursor-pointer ${
-                  tab === "tab3" &&
-                  "text-[#A46E40] border-b border-[#A46E40] font-medium"
+                className={`py-[12px] px-[20px] cursor-pointer duration-200  ${
+                  tab === "tab3" ?
+                  "text-[#A46E40] border-b border-[#A46E40] font-medium" : "border-b border-transparent font-medium"
                 }`}
               >
                 Club & Players
               </p>
               <p
                 onClick={() => setTab("tab4")}
-                className={`py-[12px] px-[20px] cursor-pointer ${
-                  tab === "tab4" &&
-                  "text-[#A46E40] border-b border-[#A46E40] font-medium"
+                className={`py-[12px] px-[20px] cursor-pointer duration-200 ${
+                  tab === "tab4" ?
+                  "text-[#A46E40] border-b border-[#A46E40] font-medium" : "border-b border-transparent font-medium"
                 }`}
               >
                 Schedule
               </p>
               <p
                 onClick={() => setTab("tab5")}
-                className={`py-[12px] px-[20px] cursor-pointer ${
-                  tab === "tab5" &&
-                  "text-[#A46E40] border-b border-[#A46E40] font-medium"
+                className={`py-[12px] px-[20px] cursor-pointer duration-200 ${
+                  tab === "tab5" ?
+                  "text-[#A46E40] border-b border-[#A46E40] font-medium" : "border-b border-transparent font-medium"
                 }`}
               >
                 Hall of Donors
