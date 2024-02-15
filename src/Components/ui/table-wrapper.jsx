7bcfe8712abfc10817652feab51e144d985fbbb0
Ashiqur Rahman Biplop <biplops22@gmail.com>
2024-02-15 14:08:13 +0600

    added schedule table full responsive

--- src/Components/ui/table-wrapper.jsx
+++ src/Components/ui/table-wrapper.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
 const TableWrapper = ({ children }) => {
   return (
-    <section className="overflow-x-auto">
+    <section className="overflow-x-auto rounded-[6px] border">
       <div className="w-[1000px]">{children}</div>
-    </section>
+    </section> 
   );
 };
 
