95800aba63ae7e36e8aebef5900959aec17bf6a1
Ashiqur Rahman Biplop <biplops22@gmail.com>
2024-02-17 00:02:31 +0600

    added some code

--- src/Components/Events/Tabs/ResultsAndStanding.jsx
+++ src/Components/Events/Tabs/ResultsAndStanding.jsx
@@ -1,5 +1,5 @@
-import AgeGroupDropDown from "../AgeGroupDropDown/AgeGroupDropDown";
 import GenderDropDown from "../GenderDropDown/GenderDropDown";
+import KUDropDown from "../KUDropDown/KUDropDown";
 import ResultTable from "../ResultTable";
 
 const ResultsAndStanding = () => {
@@ -7,40 +7,62 @@ const ResultsAndStanding = () => {
     <div className="flex flex-col gap-[16px]">
       <div className="my-[8px]">
         <div className="flex flex-col md:flex-row items-start justify-between md:justify-start md:items-center gap-[24px]">
-          <div className="flex items-center gap-[24px]">
-            <AgeGroupDropDown />
-            <GenderDropDown />
-          </div>
+          <KUDropDown />
+          <GenderDropDown />
         </div>
       </div>
       <div className="my-[8px] bg-[#F2F2F2] rounded-[6px]">
-        <div className="py-[16px] px-[16px]">
-          <div className="flex flex-col md:flex-row items-center md:gap-[64px] text-center md:text-start">
-            <div>
+        <div className="py-[16px] px-[24px]">
+          <div className="flex flex-col md:flex-row flex-wrap jc items-center gap-[48px]">
+            <div className="">
               <p className="montserrat text-[#7D7D7D] font-medium text-[16px]">
                 Top Rebound
               </p>
               <h2 className="bebas-Neue text-[20px]">Jason L</h2>
             </div>
-            <div className="divider divider-horizontal hidden md:flex"></div>
-            <div className="divider flex md:hidden"></div>
-            <div>
+            <div className="divider divider-horizontal hidden md:flex !m-0"></div>
+            <div className="divider flex md:hidden !m-0"></div>
+            <div className="">
               <p className="montserrat text-[#7D7D7D] font-medium text-[16px]">
                 Top Score
               </p>
               <h2 className="bebas-Neue text-[20px]">Juan</h2>
             </div>
-            <div className="divider divider-horizontal hidden md:flex"></div>
-            <div className="divider flex md:hidden"></div>
-            <div>
+            <div className="divider divider-horizontal hidden md:flex !m-0"></div>
+            <div className="divider flex md:hidden !m-0"></div>
+            <div className="">
               <p className="montserrat text-[#7D7D7D] font-medium text-[16px]">
                 MVP Final
               </p>
               <h2 className="bebas-Neue text-[20px]">Louis</h2>
             </div>
-            <div className="divider divider-horizontal hidden md:flex"></div>
-            <div className="divider flex md:hidden"></div>
-            <div>
+            <div className="divider divider-horizontal hidden md:flex !m-0"></div>
+            <div className="divider flex md:hidden !m-0"></div>
+            <div className="">
+              <p className="montserrat text-[#7D7D7D] font-medium text-[16px]">
+                Top Block
+              </p>
+              <h2 className="bebas-Neue text-[20px]">Louis</h2>
+            </div>
+            <div className="divider divider-horizontal hidden md:flex !m-0"></div>
+            <div className="divider flex md:hidden !m-0"></div>
+            <div className="">
+              <p className="montserrat text-[#7D7D7D] font-medium text-[16px]">
+                Top Block
+              </p>
+              <h2 className="bebas-Neue text-[20px]">Louis</h2>
+            </div>
+            <div className="divider divider-horizontal hidden md:flex !m-0"></div>
+            <div className="divider flex md:hidden !m-0"></div>
+            <div className="">
+              <p className="montserrat text-[#7D7D7D] font-medium text-[16px]">
+                Top Block
+              </p>
+              <h2 className="bebas-Neue text-[20px]">Louis</h2>
+            </div>
+            <div className="divider divider-horizontal hidden md:flex !m-0"></div>
+            <div className="divider flex md:hidden !m-0"></div>
+            <div className="">
               <p className="montserrat text-[#7D7D7D] font-medium text-[16px]">
                 Top Block
               </p>
