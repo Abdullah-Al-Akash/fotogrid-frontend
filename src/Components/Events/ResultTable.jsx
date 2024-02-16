fa1e4b93654eb6c02a4516e4f2132a928813c55e
Ashiqur Rahman Biplop <biplops22@gmail.com>
2024-02-16 15:43:58 +0600

    added table kstyle

--- src/Components/Events/ResultTable.jsx
+++ src/Components/Events/ResultTable.jsx
@@ -7,30 +7,30 @@ const ResultTable = () => {
         {/* head */}
         <thead>
           <tr className="bg-base-200 !text-[16px] !montserrat text-center">
-            <th className="border-r">Club</th>
-            <th className="border-r">Total Plays</th>
-            <th className="border-r">Total Wins</th>
-            <th className="border-r">Total Lose</th>
-            <th className="border-r">Points For</th>
-            <th className="border-r">Points Against</th>
-            <th className="border-r">Points Differences</th>
+            <th className="border-r p-[16px]">Club</th>
+            <th className="border-r p-[16px]">Total Plays</th>
+            <th className="border-r p-[16px]">Total Wins</th>
+            <th className="border-r p-[16px]">Total Lose</th>
+            <th className="border-r p-[16px]">Points For</th>
+            <th className="border-r p-[16px]">Points Against</th>
+            <th className="border-r p-[16px]">Points Differences</th>
             <th className="">Final Standings</th>
           </tr>
         </thead>
         <tbody>
           {/* row 1 */}
           <tr className="">
-            <th className="flex flex-col lg:flex-row items-center gap-[8px] border-r">
+            <th className="flex flex-col lg:flex-row items-center gap-[8px] border-r p-[12px]">
               <img src={resultTableImage} className="w-[28] h-[28]" alt="" />
               <h1>Victoria</h1>
             </th>
-            <td className="border-r text-center">10</td>
-            <td className="border-r text-center">10</td>
-            <td className="border-r text-center">0</td>
-            <td className="border-r text-center">100</td>
-            <td className="border-r text-center">50</td>
-            <td className="border-r text-center">50</td>
-            <td className="flex flex-col md:flex-row items-center gap-[8px] ">
+            <td className="border-r text-center p-[12px]">10</td>
+            <td className="border-r text-center p-[12px]">10</td>
+            <td className="border-r text-center p-[12px]">0</td>
+            <td className="border-r text-center p-[12px]">100</td>
+            <td className="border-r text-center p-[12px]">50</td>
+            <td className="border-r text-center p-[12px]">50</td>
+            <td className="flex flex-col md:flex-row items-center gap-[8px] p-[12px]">
               <img src={trophy} className="w-[20px] h-[20px]" alt="" />
               <p className="text-[16px]">Winner</p>
             </td>
@@ -41,13 +41,13 @@ const ResultTable = () => {
               <img src={resultTableImage} className="w-[28] h-[28]" alt="" />
               <h1>Victoria</h1>
             </th>
-            <td className="border-r text-center">10</td>
-            <td className="border-r text-center">10</td>
-            <td className="border-r text-center">0</td>
-            <td className="border-r text-center">100</td>
-            <td className="border-r text-center">50</td>
-            <td className="border-r text-center">-50</td>
-            <td className="flex flex-col md:flex-row items-center gap-[8px]">
+            <td className="border-r text-center p-[12px]">10</td>
+            <td className="border-r text-center p-[12px]">10</td>
+            <td className="border-r text-center p-[12px]">0</td>
+            <td className="border-r text-center p-[12px]">100</td>
+            <td className="border-r text-center p-[12px]">50</td>
+            <td className="border-r text-center p-[12px]">-50</td>
+            <td className="flex flex-col md:flex-row items-center gap-[8px] p-[12px]">
               <img src={trophy} className="w-[20px] h-[20px]" alt="" />
               <p className="text-[16px]">Winner</p>
             </td>
