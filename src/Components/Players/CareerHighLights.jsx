4d5fb7f180bec7b2084da7ebf8e2475a0c700218
AbtahiHasan <anikidpersonal@gmail.com>
2024-02-16 17:30:56 +0600

    player ui updated

--- src/Components/Players/CareerHighLights.jsx
+++ src/Components/Players/CareerHighLights.jsx
@@ -9,48 +9,48 @@ const CareerHighLights = () => {
       <h2 className="uppercase mb-[18px] text-[#F2F2F2] bebas-Neue text-[24px] leading-[32px] font-bold">
         Career highlights
       </h2>
-      <TableWrapper>
-        <table className="!w-full border-[#333333] border-[1px] !rounded-[6px]">
+      <TableWrapper borderStyle="border-[#333333] border rounded-[6px]">
+        <table className="!w-full  ">
           <thead className="bg-[#242424] rounded-[6px] overflow-hidden">
             <tr>
-              <th className="uppercase border-[1px] border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
+              <th className="uppercase border-r-[1px] border-b-[1px]  border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
                 Points
               </th>
-              <th className="uppercase border-[1px] border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
+              <th className="uppercase border-r-[1px] border-b-[1px]  border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
                 Rebounds
               </th>
-              <th className="uppercase border-[1px] border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
+              <th className="uppercase border-r-[1px] border-b-[1px]  border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
                 Assists
               </th>
-              <th className="uppercase border-[1px] border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
+              <th className="uppercase border-r-[1px] border-b-[1px]  border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
                 Blocks
               </th>
-              <th className="uppercase border-[1px] border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
+              <th className="uppercase border-r-[1px] border-b-[1px]  border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
                 Steals
               </th>
-              <th className="uppercase border-[1px] border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
+              <th className="uppercase  border-b-[1px]  border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
                 3PT
               </th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-[1px] border-[#333333]">
+              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-r-[1px] border-[#333333]">
                 24
               </td>
-              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-[1px] border-[#333333]">
+              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-r-[1px] border-[#333333]">
                 12
               </td>
-              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-[1px] border-[#333333]">
+              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-r-[1px] border-[#333333]">
                 7
               </td>
-              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-[1px] border-[#333333]">
+              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-r-[1px] border-[#333333]">
                 1
               </td>
-              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-[1px] border-[#333333]">
+              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-r-[1px] border-[#333333]">
                 8
               </td>
-              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-[1px] border-[#333333]">
+              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-[#333333]">
                 2
               </td>
             </tr>
@@ -62,48 +62,48 @@ const CareerHighLights = () => {
         <DarkDropdown options={defaultOptions} label="Event Group" />
         <DarkDropdown options={defaultOptions} label="Months" />
       </div>
-      <TableWrapper>
-        <table className="!w-full border-[#333333] border-[1px] !rounded-[6px]">
+      <TableWrapper borderStyle="border-[#333333] border rounded-[6px]">
+        <table className="!w-full border-[#333333]  !rounded-[6px]">
           <thead className="bg-[#242424] rounded-[6px] overflow-hidden">
             <tr>
-              <th className="uppercase border-[1px] border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
+              <th className="uppercase border-r-[1px] border-b-[1px] border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
                 PPG
               </th>
-              <th className="uppercase border-[1px] border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
+              <th className="uppercase border-r-[1px] border-b-[1px] border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
                 RPG
               </th>
-              <th className="uppercase border-[1px] border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
+              <th className="uppercase border-r-[1px] border-b-[1px] border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
                 APG
               </th>
-              <th className="uppercase border-[1px] border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
+              <th className="uppercase border-r-[1px] border-b-[1px] border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
                 BPG
               </th>
-              <th className="uppercase border-[1px] border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
+              <th className="uppercase border-r-[1px] border-b-[1px] border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
                 SPG
               </th>
-              <th className="uppercase border-[1px] border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
+              <th className="uppercase  border-b-[1px] border-[#333333] text-center py-[18px] text-[20px] leading-[20px] text-[#D0A885] ">
                 3PT
               </th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-[1px] border-[#333333]">
+              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-r-[1px] border-[#333333]">
                 13.9
               </td>
-              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-[1px] border-[#333333]">
+              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-r-[1px] border-[#333333]">
                 6.2
               </td>
-              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-[1px] border-[#333333]">
+              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-r-[1px] border-[#333333]">
                 2,9
               </td>
-              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-[1px] border-[#333333]">
+              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-r-[1px] border-[#333333]">
                 0.4
               </td>
-              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-[1px] border-[#333333]">
+              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-r-[1px] border-[#333333]">
                 2.9
               </td>
-              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px] border-[1px] border-[#333333]">
+              <td className="py-[14px] text-[20px] leading-[20px] text-[#D5BFA9] font-bold px-[18px]  border-[#333333]">
                 0.3
               </td>
             </tr>
--- src/Components/Players/taps/SocialMedia.jsx
+++ src/Components/Players/taps/SocialMedia.jsx
@@ -3,11 +3,11 @@ import { FaInstagram } from "react-icons/fa";
 import { FaTiktok } from "react-icons/fa6";
 const SocialMedia = () => {
   return (
-    <div className="bebas-Neue">
+    <div className="bebas-Neue py-[64px]">
       <h2 className="uppercase text-[32px] leading-[40px] font-bold  text-center">
         Avery Freeman Social Media
       </h2>
-      <div className="flex gap-[12px] justify-center items-center mt-[40px]">
+      <div className="flex gap-[12px] justify-center items-center mt-[32px]">
         <button className="bg-[#3B5998] px-[20px] py-[10px] rounded-[6px] text-white flex items-center gap-[8px] text-[20px] leading-[20px]">
           <IoLogoFacebook />
           <span>Facebook</span>
