648c7194fc7259c99340995d52b9dc6f45f5f7cd
Ashiqur Rahman Biplop <biplops22@gmail.com>
2024-02-18 18:23:39 +0600

    added some code:

--- src/Components/Players/Filter.jsx
+++ src/Components/Players/Filter.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
-import { LuInfo } from "react-icons/lu";
+import { useEffect, useRef, useState } from "react";
 import { TbFilter } from "react-icons/tb";
 import "./Filter.css";
 
 const Filter = () => {
   const [filterOpen, setFilterOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setFilterOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
   const handleFilterOption = () => {
     setFilterOpen(!filterOpen);
   };
@@ -13,7 +25,10 @@ const Filter = () => {
   };
 
   return (
-    <div className="relative text-[24px] font-bold bg-[#fff] py-[9px] px-[16px] border rounded-[4px] text-[#636363] border-[#ccc]">
+    <div
+      ref={dropdownRef}
+      className="relative text-[24px] font-bold bg-[#fff] py-[9px] px-[16px] border rounded-[4px] text-[#636363] border-[#ccc]"
+    >
       <div onClick={handleFilterOption} className="cursor-pointer">
         <TbFilter />
       </div>
@@ -21,44 +36,44 @@ const Filter = () => {
         {filterOpen && (
           <div className="cursor-pointer whitespace-nowrap absolute top-[30px] p-[32px] bg-[#fff] z-[9999] left-[50%] -translate-x-[50%] rounded-[8px] border shadow-md">
             <div className="w-[351px]">
-              <h3 class="bebas-Neue uppercase text-[#1A1A1A] text-[20px] py-[24px] font-normal leading-[32px]">
+              <h3 className="bebas-Neue uppercase text-[#1A1A1A] text-[20px] py-[24px] font-normal leading-[32px]">
                 Filters
               </h3>
-              <div class="flex items-center mb-4 ">
+              <div className="flex items-center mb-4 ">
                 <input
                   type="checkbox"
                   value=""
-                  class="w-4 h-4 text-[#D5D7D7] bg-gray-100 border-gray-300 rounded focus:ring-[#D5D7D7] dark:focus:ring-[#D5D7D7] dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  className="w-4 h-4 text-[#D5D7D7] bg-gray-100 border-gray-300 rounded focus:ring-[#D5D7D7] dark:focus:ring-[#D5D7D7] dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
                 <label
-                  for="default-checkbox"
-                  class="ms-2 text-[16px] font-normal rubik text-[#4A4A4A] dark:text-gray-300"
+                  htmlFor="default-checkbox"
+                  className="ms-2 text-[16px] font-normal rubik text-[#4A4A4A] dark:text-gray-300"
                 >
                   Filter Option #01
                 </label>
               </div>
-              <div class="flex items-center mb-4 ">
+              <div className="flex items-center mb-4 ">
                 <input
                   type="checkbox"
                   value=""
-                  class="w-4 h-4 text-[#D5D7D7] bg-gray-100 border-gray-300 rounded focus:ring-[#D5D7D7] dark:focus:ring-[#D5D7D7] dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  className="w-4 h-4 text-[#D5D7D7] bg-gray-100 border-gray-300 rounded focus:ring-[#D5D7D7] dark:focus:ring-[#D5D7D7] dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
                 <label
-                  for="default-checkbox"
-                  class="ms-2 text-[16px] font-normal rubik text-[#4A4A4A] dark:text-gray-300"
+                  htmlFor="default-checkbox"
+                  className="ms-2 text-[16px] font-normal rubik text-[#4A4A4A] dark:text-gray-300"
                 >
                   Filter Option #01
                 </label>
               </div>
-              <div class="flex items-center mb-4 ">
+              <div className="flex items-center mb-4 ">
                 <input
                   type="checkbox"
                   value=""
-                  class="w-4 h-4 text-[#D5D7D7] bg-gray-100 border-gray-300 rounded focus:ring-[#D5D7D7] dark:focus:ring-[#D5D7D7] dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  className="w-4 h-4 text-[#D5D7D7] bg-gray-100 border-gray-300 rounded focus:ring-[#D5D7D7] dark:focus:ring-[#D5D7D7] dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
                 <label
-                  for="default-checkbox"
-                  class="ms-2 text-[16px] font-normal rubik text-[#4A4A4A] dark:text-gray-300"
+                  htmlFor="default-checkbox"
+                  className="ms-2 text-[16px] font-normal rubik text-[#4A4A4A] dark:text-gray-300"
                 >
                   Filter Option #01
                 </label>
